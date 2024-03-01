d2161688c68b76a9f97ea7b3a8f4aa6661517bb6
CodeByIshika <ishika11518@gmail.com>
2024-02-29 20:00:25 -0600

    Add project files.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,83 @@
+﻿using EPBLL;
+using EPEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventPlanner.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            EventService es = new EventService();
+            var events = es.GetEvents();
+
+            return View(events);
+        }
+
+        public ActionResult CreateEventView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEventView(Events events)
+        {
+            EventService es = new EventService();
+
+            if (es.AddEventServices(events))
+            {
+                ViewBag.Message = "Event is added successfully";
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// EditEventView is used to return view with records based on EventID
+        /// </summary>
+        /// <param name="EventID"></param>
+        /// <returns></returns>
+        public ActionResult EditEventView(int EventID)
+        {
+            EventService es = new EventService();
+
+            var events = es.GetEvents().Find(x => x.EventID == EventID);
+
+
+            return View(events);
+
+        }
+
+        /// <summary>
+        /// This is used to update the record and pass that to Service and Repository
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult EditEventView(Events events)
+        {
+            EventService es = new EventService();
+
+            if (es.UpdateEventServices(events))
+            {
+                ViewBag.Message = "Event is updated successfully";
+            }
+            return View();
+        }
+
+        public ActionResult DeleteEvent(int EventID)
+        {
+            EventService es = new EventService();
+
+            if (es.DeleteEventServices(EventID))
+            {
+                ViewBag.Message = "Event is deleted successfully";
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,103 @@
+﻿using EPBLL;
+using EPEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventPlanner.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index(int EventID)
+        {
+            TempData["EventID"] = EventID;
+            ViewBag.EventID = EventID;
+            RegService rs = new RegService();
+            var reg = rs.GetParticipants(EventID);
+            return View(reg);
+            //RegService rs = new RegService();
+            //var reg = rs.GetParticipants(EventID);
+
+            //return View(reg);
+        }
+
+        public ActionResult CreateRegView()
+        {
+            EventService es = new EventService();
+            var events = es.GetEvents();
+
+            ViewBag.EventId = new SelectList(events, "EventID", "EventName");
+
+            Registration registration = new Registration();
+
+            return View(registration);
+        }
+
+        [HttpPost]
+        public ActionResult CreateRegView(Registration registration, int EventID)
+        {
+            registration.EventID = EventID;
+
+            EventService es = new EventService();
+            var events = es.GetEvents();
+
+            TempData["listOfEventsDropdown"] = events.Select(eventItem => new SelectListItem
+            {
+                Text = eventItem.EventName,
+                Value = eventItem.EventID.ToString()
+            }).ToList();
+
+            RegService rs = new RegService();
+
+            if (rs.AddRegServices(registration))
+            {
+                ViewBag.Message = "Your registration is successful";
+            }
+
+            return View(registration);
+        }
+
+
+
+        public ActionResult EditRegView(int EventID, int RegistrationID)
+        {
+            RegService rs = new RegService();
+
+            var reg = rs.GetParticipants(EventID).Find(x => x.RegistrationID == RegistrationID);
+
+            return View(reg);
+        }
+
+        /// <summary>
+        /// This is used to update the record and pass that to Service and Repository
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult EditRegView(Registration reg)
+        {
+            RegService rs = new RegService();
+
+            if (rs.UpdateRegServices(reg))
+            {
+                ViewBag.Message = "Participant information is updated successfully";
+            }
+            return View();
+        }
+
+        public ActionResult DeleteRegistration(int RegistrationID)
+        {
+            RegService rs = new RegService();
+
+            if (rs.DeleteRegServices(RegistrationID))
+            {
+                ViewBag.Message = "Registration is deleted successfully";
+            }
+            return RedirectToAction("Index", new {EventID = TempData["EventID"] });
+
+        }
+
+    }
+}
